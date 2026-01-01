775121f8635ea8845037f4b102160f0b65015ee7
777razib <kazisirazib7771@gmail.com>
2026-01-01 08:26:51 +0600

    all comoplete

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 3
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.5"
     }
 
     // Signing configurations
